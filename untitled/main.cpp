ad28ace697d7b167d610bbd1ae267c534a2dbe1e
Jessica Ortiz <jessicaortiz02b@gmail.com>
2024-07-22 09:23:52 -0600

    rompe bloques
    se cambio tambien la posicion inicial de la pelota

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -6,6 +6,7 @@
 #include <SDL_scancode.h>
 #include <string>
 #include <iostream>
+#include <vector>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -15,16 +16,34 @@ const int BALL_SIZE = 13;
 const int PADDLE_WIDTH = 100;
 const int PADDLE_HEIGHT = 20;
 const int PADDLE_SPEED = 200;
+const int BLOCK_ROWS = 5;
+const int BLOCK_COLUMNS = 10;
+const int BLOCK_WIDTH = SCREEN_WIDTH / BLOCK_COLUMNS;
+const int BLOCK_HEIGHT = 20;
 
 struct Rect {
     SDL_Rect rect = {0, 0, BALL_SIZE, BALL_SIZE};
     int vx = BALL_SPEED;
     int vy = BALL_SPEED;
-    SDL_Color color = {0xFF, 0x00, 0x00, 0xFF}; // Cambiado a rojo
+    SDL_Color color = {0xFF, 0x00, 0x00, 0xFF}; // Rojo
 };
 
-Rect ball = {{0, 0, BALL_SIZE, BALL_SIZE}, BALL_SPEED, BALL_SPEED, {0xFF, 0x00, 0x00, 0xFF}};
+struct Block {
+    SDL_Rect rect;
+    bool destroyed = false;
+};
+
+Rect ball = {{110, 110, BALL_SIZE, BALL_SIZE}, BALL_SPEED, BALL_SPEED, {0xFF, 0x00, 0x00, 0xFF}};
 SDL_Rect paddle = {SCREEN_WIDTH / 2 - PADDLE_WIDTH / 2, SCREEN_HEIGHT - PADDLE_HEIGHT - 10, PADDLE_WIDTH, PADDLE_HEIGHT};
+std::vector<Block> blocks;
+
+void createBlocks() {
+    for (int i = 0; i < BLOCK_ROWS; ++i) {
+        for (int j = 0; j < BLOCK_COLUMNS; ++j) {
+            blocks.push_back({{j * BLOCK_WIDTH, i * BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT}});
+        }
+    }
+}
 
 void renderRect(SDL_Renderer* renderer, Rect& rect) {
     SDL_SetRenderDrawColor(renderer, rect.color.r, rect.color.g, rect.color.b, rect.color.a);
@@ -34,6 +53,21 @@ void renderRect(SDL_Renderer* renderer, Rect& rect) {
 void renderPaddle(SDL_Renderer* renderer, SDL_Rect& paddle) {
     SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF); // Blanco
     SDL_RenderFillRect(renderer, &paddle);
+}
+
+void renderBlocks(SDL_Renderer* renderer) {
+    for (const auto& block : blocks) {
+        if (!block.destroyed) {
+            // Dibujar borde negro
+            SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF); // Negro
+            SDL_Rect borderRect = {block.rect.x - 1, block.rect.y - 1, block.rect.w + 2, block.rect.h + 2};
+            SDL_RenderFillRect(renderer, &borderRect);
+
+            // Dibujar bloque verde
+            SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0xFF); // Verde
+            SDL_RenderFillRect(renderer, &block.rect);
+        }
+    }
 }
 
 void renderText(SDL_Renderer* renderer, TTF_Font* font, const char* message, int x, int y) {
@@ -65,6 +99,15 @@ void update(float dT, bool& gameOver) {
         ball.vy *= -1;
         // Asegúrate de que la pelota se mueva hacia arriba después de rebotar
         ball.rect.y = paddle.y - ball.rect.h;
+    }
+
+    // Colisión con bloques
+    for (auto& block : blocks) {
+        if (!block.destroyed && SDL_HasIntersection(&ball.rect, &block.rect)) {
+            block.destroyed = true;
+            ball.vy *= -1;
+            break;
+        }
     }
 
     ball.rect.x += ball.vx * dT;
@@ -125,6 +168,8 @@ int main() {
         return -1;
     }
 
+    createBlocks();
+
     bool quit = false;
     bool gameOver = false;
     SDL_Event e;
@@ -164,6 +209,7 @@ int main() {
 
         renderRect(renderer, ball);
         renderPaddle(renderer, paddle);
+        renderBlocks(renderer);
 
         if (gameOver) {
             renderText(renderer, font, "Perdiste", SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2 - 12);
